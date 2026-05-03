ba331246077087142d11bd6faa4db5735123f189
inpfu1d <inp.fu1d@gmail.com>
2026-05-03 21:00:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ else:
     client = None
 
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+    'Accept-Language': 'en-US,en;q=0.9,ar;q=0.8',
+    'Accept': 'application/json, text/plain, */*'
 }
 
 DB_PATH = "history.db"
